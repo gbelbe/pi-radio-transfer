6c7f7d5671bc6515e1ed256518272d05694fc1f5
gaet <gbelbe@gmail.com>
2017-04-09 19:15:39 +0200

    added tx version without gui for Rpi

---
+++ src/gnuradio/tx_top.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Tx Top
+# Generated: Sun Apr  9 19:13:49 2017
+##################################################
+
+from gnuradio import audio
+from gnuradio import blocks
+from gnuradio import digital
+from gnuradio import eng_notation
+from gnuradio import filter
+from gnuradio import gr
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from optparse import OptionParser
+
+
+class tx_top(gr.top_block):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Tx Top")
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.sound_card_sample_rate = sound_card_sample_rate = 24000
+        self.samples_per_symbol = samples_per_symbol = 19
+        self.symbol_rate = symbol_rate = sound_card_sample_rate/samples_per_symbol
+        
+        self.variable_rrc_filter_taps = variable_rrc_filter_taps = firdes.root_raised_cosine(15, sound_card_sample_rate, symbol_rate, 0.5, 70)
+          
+        self.bits_per_symbol = bits_per_symbol = 1
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.interp_fir_filter_xxx_0 = filter.interp_fir_filter_fff(samples_per_symbol, (variable_rrc_filter_taps))
+        self.interp_fir_filter_xxx_0.declare_sample_delay(0)
+        self.digital_hdlc_framer_pb_0 = digital.hdlc_framer_pb('packet_len')
+        self.digital_chunks_to_symbols_xx_0 = digital.chunks_to_symbols_bf(([-1,1]), 1)
+        self.blocks_wavfile_sink_0 = blocks.wavfile_sink('tx_signal.wav', 1, int(sound_card_sample_rate), 8)
+        self.blocks_tagged_stream_to_pdu_0 = blocks.tagged_stream_to_pdu(blocks.byte_t, 'packet_len')
+        self.blocks_stream_to_tagged_stream_0 = blocks.stream_to_tagged_stream(gr.sizeof_char, 1, 32, "packet_len")
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_char*1, '/home/gbelbe/Develop/pi-radio-transfer/src/gnuradio/test_src_file.txt', False)
+        self.audio_sink_0 = audio.sink(int(sound_card_sample_rate), '', True)
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.msg_connect((self.blocks_tagged_stream_to_pdu_0, 'pdus'), (self.digital_hdlc_framer_pb_0, 'in'))    
+        self.connect((self.blocks_file_source_0, 0), (self.blocks_stream_to_tagged_stream_0, 0))    
+        self.connect((self.blocks_stream_to_tagged_stream_0, 0), (self.blocks_tagged_stream_to_pdu_0, 0))    
+        self.connect((self.digital_chunks_to_symbols_xx_0, 0), (self.interp_fir_filter_xxx_0, 0))    
+        self.connect((self.digital_hdlc_framer_pb_0, 0), (self.digital_chunks_to_symbols_xx_0, 0))    
+        self.connect((self.interp_fir_filter_xxx_0, 0), (self.audio_sink_0, 0))    
+        self.connect((self.interp_fir_filter_xxx_0, 0), (self.blocks_wavfile_sink_0, 0))    
+
+    def get_sound_card_sample_rate(self):
+        return self.sound_card_sample_rate
+
+    def set_sound_card_sample_rate(self, sound_card_sample_rate):
+        self.sound_card_sample_rate = sound_card_sample_rate
+        self.set_symbol_rate(self.sound_card_sample_rate/self.samples_per_symbol)
+
+    def get_samples_per_symbol(self):
+        return self.samples_per_symbol
+
+    def set_samples_per_symbol(self, samples_per_symbol):
+        self.samples_per_symbol = samples_per_symbol
+        self.set_symbol_rate(self.sound_card_sample_rate/self.samples_per_symbol)
+
+    def get_symbol_rate(self):
+        return self.symbol_rate
+
+    def set_symbol_rate(self, symbol_rate):
+        self.symbol_rate = symbol_rate
+
+    def get_variable_rrc_filter_taps(self):
+        return self.variable_rrc_filter_taps
+
+    def set_variable_rrc_filter_taps(self, variable_rrc_filter_taps):
+        self.variable_rrc_filter_taps = variable_rrc_filter_taps
+        self.interp_fir_filter_xxx_0.set_taps((self.variable_rrc_filter_taps))
+
+    def get_bits_per_symbol(self):
+        return self.bits_per_symbol
+
+    def set_bits_per_symbol(self, bits_per_symbol):
+        self.bits_per_symbol = bits_per_symbol
+
+
+def main(top_block_cls=tx_top, options=None):
+
+    tb = top_block_cls()
+    tb.start()
+    tb.wait()
+
+
+if __name__ == '__main__':
+    main()
